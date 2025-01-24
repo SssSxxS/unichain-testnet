fb0d40867ed2baef4501a7df5a169ce7c92a6ed9
SssSxxS <sssx0sxxx@gmail.com>
2025-01-25 02:01:19 +0300

    commit

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -1,7 +1,7 @@
 import { sleep } from "bun"
 import { logger } from "./logger"
 
-export const getRandomNumFloor = (min: number, max: number) => Math.floor(Math.random() * (max - min) + min)
+export const getRandomNumFloor = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
 
 export const getRandomNumToFixed = (min: number, max: number, toFixed: number) =>
   (Math.random() * (max - min) + min).toFixed(toFixed)
